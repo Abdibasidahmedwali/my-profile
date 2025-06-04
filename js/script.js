154623c35bc783826ef2f26c7f0e91d5bde2c28b
Abdibasidahmedwali <157267718+Abdibasidahmedwali@users.noreply.github.com>
2025-06-04 14:11:51 +0300

    First commit - my portfolio files

---
+++ js/script.js
@@ -0,0 +1,72 @@
+GamepadButton.prototype.getValue = function() {
+    return this.pressed ? 1 : 0;
+};
+GamepadButton.prototype.getValue = function() { 
+    return this.pressed ? 1 : 0;
+};
+GamepadButton.prototype.getValue = function() {
+    return this.pressed ? 1 : 0;
+};
+GamepadButton.prototype.getValue = function() {
+    return this.pressed ? 1 : 0;
+};
+GamepadButton.prototype.getValue = function() {
+    return this.pressed ? 1 : 0;
+};
+GamepadButton.prototype.getValue = function() {
+    return this.pressed ? 1 : 0;
+}
+GamepadButton.prototype.getValue = function() {
+    return this.pressed ? 1 : 0;
+}
+GamepadButton.prototype.getValue = function() {
+    return this.pressed ? 1 : 0;
+}
+GamepadButton.prototype.getValue = function() {
+    return this.pressed ? 1 : 0;
+}
+GamepadButton.prototype.getValue = function() {
+    return this.pressed ? 1 : 0;
+}   
+GamepadButton.prototype.getValue = function() {
+    return this.pressed ? 1 : 0;
+}
+getComputedStyle = (function() {
+    var originalGetComputedStyle = window.getComputedStyle;
+    return function(element, pseudoElement) {
+        if (element instanceof HTMLElement) {
+            return originalGetComputedStyle.call(window, element, pseudoElement);
+        } else {
+            return null;
+        }
+    };
+})();
+function getElementById(id) {
+    return document.getElementById(id);
+}   
+function getElementsByClassName(className) {
+    return document.getElementsByClassName(className);
+}
+function getElementsByTagName(tagName) {
+    return document.getElementsByTagName(tagName);
+}
+function getElementsByName(name) {  
+    return document.getElementsByName(name);
+}
+function getElementsBySelector(selector) {
+    return document.querySelectorAll(selector);
+}
+function getElementsBySelectorAll(selector) {
+    return document.querySelectorAll(selector);
+}
+function getElementsBySelectorAll(selector) {
+    return document.querySelectorAll(selector);
+}
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', function() {
+        console.log('Element clicked:', this);
+    });
+}
+function getElementsBySelectorAll(selector) {
+    return document.querySelectorAll(selector);
+}
